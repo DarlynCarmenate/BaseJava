ad695d13bbb9250cb26d57e5b1b3a17cb9f965f6
darlyncarmenate <darlyncarmenate@gmail.com>
2022-12-20 21:25:38 +0200

    The entrance test for Basejava course added

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,47 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int numResumes;
 
     void clear() {
+        Arrays.fill(storage, 0, numResumes, null);
+        numResumes = 0;
     }
 
     void save(Resume r) {
+        storage[numResumes] = r;
+        numResumes++;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < numResumes; i++) {
+            if (uuid.equals(storage[i].toString())) return storage[i];
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < numResumes; i++) {
+            if (uuid.equals(storage[i].toString())) {
+                numResumes--;
+                System.arraycopy(storage, i + 1, storage, i, numResumes - i);
+                storage[numResumes] = null;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, numResumes);
     }
 
     int size() {
-        return 0;
+        return numResumes;
     }
 }
