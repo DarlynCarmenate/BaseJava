fb67e86cde7df43c98811af172769cb92bb40b5e
darlyncarmenate <darlyncarmenate@gmail.com>
2022-12-25 22:42:12 +0200

    Exception warnings added to ArrayStorage.java

--- src/com/basejava/webapp/MainTestArrayStorage.java
+++ src/com/basejava/webapp/MainTestArrayStorage.java
@@ -3,9 +3,6 @@ package com.basejava.webapp;
 import com.basejava.webapp.model.Resume;
 import com.basejava.webapp.storage.ArrayStorage;
 
-/**
- * Test for your com.basejava.webapp.storage.ArrayStorage implementation
- */
 public class MainTestArrayStorage {
     static final ArrayStorage ARRAY_STORAGE = new ArrayStorage();
 
--- src/com/basejava/webapp/storage/ArrayStorage.java
+++ src/com/basejava/webapp/storage/ArrayStorage.java
@@ -9,9 +9,16 @@ public class ArrayStorage {
     private int numResumes;
 
     public void update(String uuid, String newUuid) {
-        for (int i = 0; i < numResumes; i++) {
-            if (uuid.equals(storage[i].toString())) {
-                storage[i].setUuid(newUuid);
+        if (numResumes > 10000)
+            System.out.println("The storage overflow");
+        if (checkUuid(uuid) == 0) {
+            System.out.println("There's no " + uuid + " for updating");
+        } else {
+            for (int i = 0; i < numResumes; i++) {
+                if (uuid.equals(storage[i].toString())) {
+                    storage[i].setUuid(newUuid);
+                    System.out.println("Storage updated");
+                }
             }
         }
     }
@@ -19,14 +26,26 @@ public class ArrayStorage {
     public void clear() {
         Arrays.fill(storage, 0, numResumes, null);
         numResumes = 0;
+        System.out.println("Storage cleared");
     }
 
     public void save(Resume resume) {
+        for (int i = 0; i < numResumes; i++) {
+            if (resume.equals(storage[i])) {
+                System.out.println("The resume already exists");
+                break;
+            }
+        }
         storage[numResumes] = resume;
+        System.out.println("Resume " + storage[numResumes].getUuid() + " saved");
         numResumes++;
     }
 
     public Resume get(String uuid) {
+        if (checkUuid(uuid) == 0) {
+            System.out.println("There's no " + uuid + " to get");
+            return null;
+        }
         for (int i = 0; i < numResumes; i++) {
             if (uuid.equals(storage[i].toString()))
                 return storage[i];
@@ -34,12 +53,17 @@ public class ArrayStorage {
         return null;
     }
 
-    public void delete(String uuid) {
+    public void delete(String uuid){
+        if (checkUuid(uuid) == 0) {
+            System.out.println("There's no " + uuid + " for deleting");
+            return;
+        }
         for (int i = 0; i < numResumes; i++) {
             if (uuid.equals(storage[i].toString())) {
-                numResumes--;
-                System.arraycopy(storage, i + 1, storage, i, numResumes - i);
+                storage[i] = storage[numResumes - 1];
                 storage[numResumes] = null;
+                System.out.println("Resume " + storage[i].getUuid() + " deleted");
+                numResumes--;
             }
         }
     }
@@ -53,5 +77,13 @@ public class ArrayStorage {
 
     public int size() {
         return numResumes;
+    }
+
+    private int checkUuid(String uuid) {
+        for (int i = 0; i < numResumes; i++) {
+            if (uuid.equals(storage[i].toString()))
+                return 1;
+        }
+        return 0;
     }
 }
